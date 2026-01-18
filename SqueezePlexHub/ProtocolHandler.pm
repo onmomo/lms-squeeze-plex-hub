ee37f089f526b372065b7ac86d2b43a119ae1536
onmomo <mail@christian-moser.ch>
2026-01-18 20:11:48 +0100

    only add supported metadata tags

--- SqueezePlexHub/ProtocolHandler.pm
+++ SqueezePlexHub/ProtocolHandler.pm
@@ -11,7 +11,6 @@ use XML::Simple qw(XMLin);
 use Slim::Music::Info;
 use Time::HiRes ();
 
-
 use Slim::Utils::Cache;
 use Slim::Utils::Log;
 
@@ -20,8 +19,9 @@ my $log   = logger('plugin.squeezeplexhub');
 
 # Register our URL handler for URLs containing squeezePlexHub_rk param
 use constant SPH_URL_REGEXP => qr/[?&]squeezePlexHub_rk=\d+/i;
-Slim::Player::ProtocolHandlers->registerURLHandler(SPH_URL_REGEXP, __PACKAGE__)
-    if Slim::Player::ProtocolHandlers->can('registerURLHandler');
+Slim::Player::ProtocolHandlers->registerURLHandler( SPH_URL_REGEXP,
+    __PACKAGE__ )
+  if Slim::Player::ProtocolHandlers->can('registerURLHandler');
 
 sub contentType     { 'sph' }
 sub canDirectStream { 0 }
@@ -35,46 +35,52 @@ sub requestString {
 sub explodePlaylist {
     my ( $class, $client, $uri, $cb ) = @_;
 
-    my $url_res     = _parse_stream_url($uri);
+    my $url_res   = _parse_stream_url($uri);
     my $url_clean = $url_res->{clean};
 
-    unless (defined $url_clean && $url_clean ne '') {
-        $log->error("SPH explodePlaylist: clean URL is empty, returning original URI");
-        $cb->([ $uri]);
+    unless ( defined $url_clean && $url_clean ne '' ) {
+        $log->error(
+            "SPH explodePlaylist: clean URL is empty, returning original URI");
+        $cb->( [$uri] );
         return;
     }
 
     # Always return the playable URL immediately
     # Important: use the clean URL without our rk param in order to skip this protocol handler for the actual playback
-    $cb->([ $url_clean ]);    
+    $cb->( [$url_clean] );
 
     # Async fetch metadata to update current track info
-    _fetch_plex_track_metadata(sub {
-        my ($m) = @_;
-        return unless $m && ref($m) eq 'HASH';
+    _fetch_plex_track_metadata(
+        sub {
+            my ($m) = @_;
+            return unless $m && ref($m) eq 'HASH';
 
-        my $tags = {
-            title    => $m->{title}    || '',
-            artist   => $m->{artist}   || '',
-            album    => $m->{album}    || '',
-            year     => $m->{year}     || '',
-            duration => $m->{duration} || 0,
-            cover    => $m->{cover}    || $m->{icon},
-            icon     => $m->{icon},
-            type     => 'Plex (SqueezePlexHub)',
-        };
+            my $meta = {
+                # TODO check if we can also add artist and album
+                title => $m->{title} || '',
+                year  => $m->{year}  || '',
+                # duration must be in "secs" (seconds, or hh:mm:ss string)
+                secs => $m->{duration} || 0,                            
+                # artwork URL
+                cover => $m->{cover} || $m->{icon},
+            }; 
+        
+            # TODO consider caching per URL to avoid redundant fetches            
+            Slim::Music::Info::setRemoteMetadata( $url_clean, $meta );
 
-        # Update current track metadata      
-        # TODO consider caching per URL to avoid redundant fetches
-        # TODO check if remoteMetadata does support all these tags
-        Slim::Music::Info::setRemoteMetadata($url_clean, $tags);
-
-        eval {
-            $client->currentPlaylistUpdateTime( Time::HiRes::time() ) if $client;
-            Slim::Control::Request::notifyFromArray( $client, [ 'newmetadata' ] ) if $client;
-            1;
-        };
-    }, $url_res->{base}, $url_res->{token}, $url_res->{rk});
+            eval {
+                $client->currentPlaylistUpdateTime( Time::HiRes::time() )
+                  if $client;
+                Slim::Control::Request::notifyFromArray( $client,
+                    ['newmetadata'] )
+                  if $client;
+                1;
+            };
+        },
+        $url_res->{base},
+        $url_res->{token},
+        $url_res->{rk}
+    );
 }
 
 sub _parse_stream_url {
@@ -97,10 +103,10 @@ sub _parse_stream_url {
     }
 
     return {
-        rk        => $rk,
-        token     => $token,
-        base      => $base,
-        clean     => $clean_url
+        rk    => $rk,
+        token => $token,
+        base  => $base,
+        clean => $clean_url
     };
 }
 
@@ -122,21 +128,24 @@ sub _fetch_plex_track_metadata {
                 $data = XMLin( $content, ForceArray => 1, KeyAttr => [] );
                 1;
             } or do {
-                $log->warn("SPH: failed to parse Plex XML for rk=$rk: $@");
+                $log->error("SPH: failed to parse Plex XML for rk=$rk: $@");
                 $cb->(undef);
                 return;
             };
 
-            my $track = ($data->{Track} && ref($data->{Track}) eq 'ARRAY') ? $data->{Track}[0] : undef;
+            my $track =
+              ( $data->{Track} && ref( $data->{Track} ) eq 'ARRAY' )
+              ? $data->{Track}[0]
+              : undef;
 
-            if (!$track) {
+            if ( !$track ) {
                 $cb->(undef);
                 return;
             }
 
-            my $title  = $track->{title} || '';
+            my $title  = $track->{title}            || '';
             my $artist = $track->{grandparentTitle} || '';
-            my $album  = $track->{parentTitle} || '';
+            my $album  = $track->{parentTitle}      || '';
 
             my $tracknum = '';
             $tracknum = $track->{index} if defined $track->{index};
@@ -145,16 +154,17 @@ sub _fetch_plex_track_metadata {
             $disc = $track->{parentIndex} if defined $track->{parentIndex};
 
             my $year = '';
-            if (defined $track->{parentYear}) {
+            if ( defined $track->{parentYear} ) {
                 $year = $track->{parentYear};
-            }
-            elsif (defined $track->{year}) {
+            }            
+
+            elsif ( defined $track->{year} ) {
                 $year = $track->{year};
             }
 
             my $duration = 0;
-            if ($track->{duration} && $track->{duration} =~ /^\d+$/) {
-                $duration = int($track->{duration} / 1000);
+            if ( $track->{duration} && $track->{duration} =~ /^\d+$/ ) {
+                $duration = int( $track->{duration} / 1000 );
             }
 
             my $thumb = $track->{thumb} || $track->{parentThumb};
@@ -163,23 +173,26 @@ sub _fetch_plex_track_metadata {
             if ($thumb) {
                 $icon = $base . $thumb;
                 if ( $token && $icon !~ /X-Plex-Token=/i ) {
-                    $icon .= ( $icon =~ /\?/ ? '&' : '?' ) . 'X-Plex-Token=' . $token;
+                    $icon .=
+                      ( $icon =~ /\?/ ? '&' : '?' ) . 'X-Plex-Token=' . $token;
                 }
             }
 
             $log->debug("SPH: fetched Plex metadata for rk=$rk: title='$title', artist='$artist', album='$album', year='$year', duration=$duration, tracknum='$tracknum', disc='$disc', icon='$icon'");
 
-            $cb->({
-                title    => $title,
-                artist   => $artist,
-                album    => $album,
-                year     => $year,
-                duration => $duration,
-                tracknum => $tracknum,
-                disc     => $disc,
-                icon     => $icon,
-                cover    => $icon,
-            });
+            $cb->(
+                {
+                    title    => $title,
+                    artist   => $artist,
+                    album    => $album,
+                    year     => $year,
+                    duration => $duration,
+                    tracknum => $tracknum,
+                    disc     => $disc,
+                    icon     => $icon,
+                    cover    => $icon                    
+                }
+            );
         },
         sub {
             my ($http) = @_;
